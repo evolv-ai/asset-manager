6be4760abf6b5e72eee97f29e0ef8e8c30ce2b51
Tyler Foster <tyler.g.foster@gmail.com>
2020-03-29 09:57:22 -0700

    Honoring doNotTrack

--- src/webloader.js
+++ src/webloader.js
@@ -46,65 +46,80 @@ function injectStylesheet(endpoint, env, uid) {
 	document.head.appendChild(stylesheet);
 }
 
-if (!window.evolv) {
-	window.evolv = {};
-}
-
-const evolv = window.evolv;
-
-if (!evolv.store) {
-	evolv.store = function(key, value, session) {
-		(session ? window.sessionStorage : window.localStorage).setItem('evolv_' + key, value);
+function main() {
+	if (!window.evolv) {
+		window.evolv = {};
 	}
-}
 
-if (!evolv.retrieve) {
-	window.evolv.retrieve = function(key, session) {
-		return (session ? window.sessionStorage : window.localStorage).getItem('evolv_' + key);
+	const evolv = window.evolv;
+
+	if (!evolv.store) {
+		evolv.store = function (key, value, session) {
+			(session ? window.sessionStorage : window.localStorage).setItem('evolv_' + key, value);
+		}
 	}
+
+	if (!evolv.retrieve) {
+		window.evolv.retrieve = function (key, session) {
+			return (session ? window.sessionStorage : window.localStorage).getItem('evolv_' + key);
+		}
+	}
+
+	const script = currentScript();
+	const env = script.dataset.env;
+	let js = script.dataset.js;
+	let css = script.dataset.css;
+	let endpoint = script.dataset.endpoint || 'https://participants.evolv.ai/v1';
+	if (!env) {
+		throw new Error('Evolv: Environment not specified');
+	}
+	const uid = script.dataset.uid || ensureId(evolv, 'uid', false);
+	const sid = script.dataset.sid || ensureId(evolv, 'sid', true);
+
+	js = !js || js === 'true';
+	css = !css || css === 'true';
+
+	if (js) {
+		injectScript(endpoint, env, uid);
+	}
+
+	if (css) {
+		injectStylesheet(endpoint, env, uid);
+	}
+
+	let client = evolv.client;
+	if (!client) {
+		let options = {
+			env: env,
+			endpoint: endpoint
+		};
+		client = new EvolvClient(options);
+		client.initialize(uid, sid);
+		Object.defineProperty(window.evolv, 'client', {
+			get: function () {
+				return client
+			}
+		});
+		Object.defineProperty(window.evolv, 'context', {
+			get: function () {
+				return client.context
+			}
+		});
+	}
+
+	client.context.set('webloader.js', js);
+	client.context.set('webloader.css', css);
+
+	const assetManager = new EvolvAssetManager(client);
+
+	Object.defineProperty(window.evolv, 'assetManager', {
+		get: function () {
+			return assetManager
+		}
+	});
 }
 
-const script = currentScript();
-const env = script.dataset.env;
-let js = script.dataset.js;
-let css = script.dataset.css;
-let uid = script.dataset.uid;
-let sid = script.dataset.sid;
-let endpoint = script.dataset.endpoint || 'https://participants.evolv.ai/v1';
-
-if (!env) {
-	throw new Error('Evolv: Environment not specified');
+// If the user has requested not to be tracked, or the browser is older than ie11, bail out.
+if (!window.doNotTrack && typeof Map !== 'undefined') {
+	main();
 }
-
-uid = uid || ensureId(evolv, 'uid', false);
-sid = sid || ensureId(evolv, 'sid', true);
-
-js = !js || js === 'true';
-css = !css || css === 'true';
-
-if (js) {
-	injectScript(endpoint, env, uid);
-}
-
-if (css) {
-	injectStylesheet(endpoint, env, uid);
-}
-
-let client = evolv.client;
-if (!client) {
-	let options = {
-		env: env,
-		endpoint: endpoint
-	};
-	client = new EvolvClient(options);
-	client.initialize(uid, sid);
-	Object.defineProperty(window.evolv, 'client', { get: function () { return client }});
-	Object.defineProperty(window.evolv, 'context', { get: function () { return client.context }});
-}
-
-client.context.set('webloader.js', js);
-client.context.set('webloader.css', css);
-
-const assetManager = new EvolvAssetManager(client);
-
-Object.defineProperty(window.evolv, 'assetManager', { get: function () { return assetManager }});
