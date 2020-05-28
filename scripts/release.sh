d552c4b782a9463aa2ce91e3d9e1c420b8547cb6
Frazer Bayley <bayley.frazer@gmail.com>
2020-05-28 16:27:39 -0700

    fix: being more explicit

--- scripts/release.sh
+++ scripts/release.sh
@@ -8,11 +8,11 @@ npm run lint
 if [[ "$1" == "prod" ]]; then
 	echo "Releasing asset-manager to prod"
 	# npm run release
-	echo "Done releasing asset-manager"
+	echo "Done releasing asset-manager to prod"
 elif [[ "$1" == "staging" ]]; then
 	echo "Releasing asset-manager to staging"
 	npm run prerelease
-	echo "Done releasing asset-manager"
+	echo "Done releasing asset-manager to staging"
 else
 	echo "EVOLV_STAGE '$1' not recognized. Doing nothing."
 fi
