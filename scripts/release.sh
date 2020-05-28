397c8662f29a3b1cb7ab4e8b4707a9af9269b8e1
Frazer Bayley <bayley.frazer@gmail.com>
2020-05-28 16:26:50 -0700

    feat: adding real script

---
+++ scripts/release.sh
@@ -0,0 +1,18 @@
+#!/bin/sh -l
+
+echo "Beginning asset-manager release to $1!"
+
+npm test
+npm run lint 
+
+if [[ "$1" == "prod" ]]; then
+	echo "Releasing asset-manager to prod"
+	# npm run release
+	echo "Done releasing asset-manager"
+elif [[ "$1" == "staging" ]]; then
+	echo "Releasing asset-manager to staging"
+	npm run prerelease
+	echo "Done releasing asset-manager"
+else
+	echo "EVOLV_STAGE '$1' not recognized. Doing nothing."
+fi
