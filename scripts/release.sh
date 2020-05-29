2e1ef1f2a16b14c4a6a11fe58c20f18507aa5217
Frazer Bayley <bayley.frazer@gmail.com>
2020-05-28 17:13:45 -0700

    fix: no quotes

--- scripts/release.sh
+++ scripts/release.sh
@@ -6,12 +6,12 @@ npm ci
 npm test
 npm run lint 
 
-if [[ "$EVOLV_STAGE" == "prod" ]]; then
+if [[ $EVOLV_STAGE == "prod" ]]; then
 	pip install awscli
 	echo "Releasing asset-manager to prod"
 	# npm run release
 	echo "Done releasing asset-manager to prod"
-elif [[ "$EVOLV_STAGE" == "staging" ]]; then
+elif [[ $EVOLV_STAGE == "staging" ]]; then
 	pip install awscli
 	echo "Releasing asset-manager to staging"
 	npm run prerelease
