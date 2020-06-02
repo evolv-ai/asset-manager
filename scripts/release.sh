0806ad0e9810a8b0ce123b33dfde43df302345f2
Frazer Bayley <bayley.frazer@gmail.com>
2020-06-01 18:13:43 -0700

    fix: some of Matts comments

--- scripts/release.sh
+++ scripts/release.sh
@@ -14,7 +14,7 @@ if [[ "$EVOLV_STAGE" == "prod" ]]; then
 elif [[ "$EVOLV_STAGE" == "staging" ]]; then
 	pip install awscli
 	echo "Releasing asset-manager to staging"
-	npm run prerelease
+	npm run staging:release
 	echo "Done releasing asset-manager to staging"
 else
 	echo "EVOLV_STAGE '$EVOLV_STAGE' not recognized. Doing nothing."
