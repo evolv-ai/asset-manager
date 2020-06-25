960ffae5bd7330da3bba67e38d5666d1390165e6
Roman Mamchyk <roman.mamchyk@gmail.com>
2020-06-25 18:31:42 +0300

    fix: IE does not supoort template strings (#46)

--- src/modes/qa.mode.js
+++ src/modes/qa.mode.js
@@ -1,5 +1,6 @@
 export const QA_RE = 'evolvCandidateToken=(([0-9]+)_([0-9]+)_([0-9a-z]+))';
-const QA_RE_REPLACE = `#?&?${QA_RE}`;
+// template strings is not supported by IE
+const QA_RE_REPLACE = '#?&?' + QA_RE;
 
 export default {
 	shouldActivate(environmentId) {
