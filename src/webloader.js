e1e9684d81e609a8e4872892bff38c9c007a2a76
Frazer Bayley <bayley.frazer@gmail.com>
2020-06-25 11:33:31 -0700

    feat: enable analytics endpoint

--- src/webloader.js
+++ src/webloader.js
@@ -102,7 +102,8 @@ function main() {
 			environment: env,
 			endpoint: endpoint,
 			version: version,
-			autoConfirm: false
+			autoConfirm: false,
+			analytics: true
 		};
 		client = new EvolvClient(options);
 		client.initialize(uid, sid);
