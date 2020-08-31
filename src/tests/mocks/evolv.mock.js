a84f15024907f6372dd3233bf830abb3da7c064f
Rob Severn <robert.severn@evolv.ai>
2020-08-31 10:42:59 -0700

    fix: previous keys apply
    were not clearing the functions sets, so we were accumulating if the page was not refreshed
    
    RKT-7874

--- src/tests/mocks/evolv.mock.js
+++ src/tests/mocks/evolv.mock.js
@@ -5,6 +5,9 @@ class EvolvMock {
 		this.initializations = 0;
 		this.keys = keys;
 		this.context = new EvolvContextMock();
+
+		this._listener;
+		this._prefix = '';
 	}
 
 	initialize(uid, sid, remoteContext, localContext) {
@@ -20,17 +23,28 @@ class EvolvMock {
 	}
 
 	getActiveKeys(prefix) {
+		this._prefix = prefix;
 		const keys = this.keys;
 		const promise = new Promise((resolve, reject) => {
 			resolve(true);
 		});
+		let that = this;
 		promise.listen = function(listener) {
+			that._listener = listener;
 			listener({
 				current: keys.filter(key => key.startsWith(prefix)),
 				previous: []
 			})
 		};
 		return promise;
+	}
+
+	fireActiveKeyListenerNewKeys(keys) {
+		this._listener({
+			current: keys.filter(key => key.startsWith(this._prefix)),
+			previous: this.keys || []
+		});
+		this.keys = keys;
 	}
 }
 
