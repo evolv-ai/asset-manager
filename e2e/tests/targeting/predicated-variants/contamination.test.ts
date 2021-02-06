7a667a5799a9c2baa5fd1f23e8898ac38c440ba2
Matt Strom <matt.strom@evolv.ai>
2021-02-05 18:09:51 -0800

    test: replace spies with request loggers

--- e2e/tests/targeting/predicated-variants/contamination.test.ts
+++ e2e/tests/targeting/predicated-variants/contamination.test.ts
@@ -1,35 +1,24 @@
 /// <reference path="../../../types.d.ts" />
 
-import { SinonSpy } from 'sinon';
-import * as sinon from 'sinon';
-import { RequestMock } from 'testcafe';
+import { RequestLogger } from 'testcafe';
 
 import { buildRequestHooks } from '../../../helpers';
 import hooks from './contamination.hooks';
 
 
-let spy: SinonSpy;
+const logger = RequestLogger(/\/v1\/.+\/events/, {
+	logRequestBody: true,
+	stringifyRequestBody: true
+});
 
 fixture `Predicated Variant Targeting > Contaminations`
     .page `http://localhost:9090/tests/targeting/index.html`
-	.meta({ spies: true })
-	.beforeEach(async () => {
-		spy = sinon.spy();
-	})
     .requestHooks([
 	    ...buildRequestHooks(hooks),
-	    RequestMock()
-		    .onRequestTo(/\/v1\/.+\/events/)
-		    .respond((req: any, res: any) => {
-			    const body = JSON.parse(req.body.toString());
-			    spy(body);
-		    })
+	    logger
     ]);
 
 test(`should fire contamination event after synchronous variant error`, async t => {
-	// Preconditions
-	await t.expect(spy.notCalled).ok();
-
 	// Act
 	await t.eval(function() {
 		evolv.context.set('state', 'TX');
@@ -37,23 +26,30 @@ test(`should fire contamination event after synchronous variant error`, async t 
 
 	// Assert
     await t
-	    .expect(spy.called).ok()
-	    .expect(spy.lastCall.args[0].type).eql('contamination')
-	    .expect(spy.lastCall.args[0].contaminationReason.details).eql('Don\'t mess with Texas');
+		.expect(logger.count(() => true))
+	    .gte(1)
+		.expect(logger.contains(result => {
+			const data = JSON.parse(result.request.body);
+			return data.type === 'contamination'
+				&& data.contaminationReason.details === 'Don\'t mess with Texas';
+		}))
+	    .ok();
 });
 
 test(`should fire contamination event after asynchronous variant rejection`, async t => {
-	// Preconditions
-	await t.expect(spy.notCalled).ok();
-
 	// Act
 	await t.eval(function() {
 		evolv.context.set('state', 'CA');
 	});
 
 	// Assert
-    await t
-	    .expect(spy.called).ok()
-	    .expect(spy.lastCall.args[0].type).eql('contamination')
-	    .expect(spy.lastCall.args[0].contaminationReason.details).eql('Later dude');
+	await t
+		.expect(logger.count(() => true))
+		.gte(1)
+		.expect(logger.contains(result => {
+			const data = JSON.parse(result.request.body);
+			return data.type === 'contamination'
+				&& data.contaminationReason.details === 'Later dude';
+		}))
+		.ok();
 });
