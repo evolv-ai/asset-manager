bdb4663b02da65f1a2f97a58df89431a3b2f7c76
Matt Strom <5853934+mattstrom@users.noreply.github.com>
2021-02-08 09:25:25 -0800

    test: account for default colors in different browsers (#175)
    Co-authored-by: Matt Strom <matt.strom@evolv.ai>

--- e2e/helpers/get-default-colors.ts
+++ e2e/helpers/get-default-colors.ts
@@ -1,15 +1,15 @@
 /// <reference path="../../node_modules/testcafe/ts-defs/index.d.ts" />
 
 export function getDefaultColors(browser: Browser) {
-	if (browser.alias === 'firefox') {
+	if (browser.name === 'Firefox') {
 		return {
 			button: 'rgb(0, 0, 0)',
 			buttonBackground: 'rgb(240, 240, 240)'
 		};
-	} else if (browser.alias === 'ie') {
+	} else if (browser.name === 'Internet Explorer') {
 		return {
 			button: 'rgb(33, 33, 33)',
-			buttonBackground: 'rgb(239, 239, 239)'
+			buttonBackground: 'rgb(240, 240, 240)'
 		};
 	} else {
 		return {
