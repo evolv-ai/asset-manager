5a24860b3016f1c9158806b653448b0d0d4b50cf
Matt Strom <matt.strom@evolv.ai>
2021-04-07 11:51:27 -0700

    feat: support for "Wait for Elements"
    [RKT-8750]

---
+++ src/helpers.js
@@ -0,0 +1,60 @@
+
+const Helpers = /** @class */ (function() {
+
+    function Helpers() {
+        this.disposers = [];
+    }
+
+    /**
+     * @param {string[]} selectors
+     * @param {function(*?):void} callback
+     */
+    Helpers.prototype.waitFor = function(selectors, callback) {
+        this.dispose();
+
+        const predicate = function() {
+            return selectors.every(function(selector) {
+                return document.querySelectorAll(selector).length !== 0;
+            });
+        };
+
+        let timer;
+
+        function run() {
+            cancelAnimationFrame(timer);
+
+            try {
+                if (predicate()) {
+                    callback(null);
+                } else {
+                    timer = requestAnimationFrame(run);
+                }
+            } catch (err) {
+                callback(err);
+            }
+        }
+
+        run();
+
+        return this.addDisposer(function() {
+            cancelAnimationFrame(timer);
+        });
+    };
+
+    Helpers.prototype.addDisposer = function(disposer) {
+        this.disposers.push(disposer);
+        return disposer;
+    }
+
+    Helpers.prototype.dispose = function() {
+        this.disposers.forEach(function(disposer) {
+            typeof disposer === 'function' && disposer();
+        });
+
+        this.disposers = [];
+    };
+
+    return Helpers;
+})();
+
+export { Helpers };
