9ef58596a65a0228a1fbde7a694b442dafbf5dd7
rmamchyk <roman.mamchyk@gmail.com>
2021-06-14 13:10:45 +0000

    fix: block and qa modes don't work when consent required

--- src/modes/qa.mode.js
+++ src/modes/qa.mode.js
@@ -3,18 +3,18 @@ export const QA_RE = 'evolvCandidateToken=(([0-9]+)_([0-9]+)_([0-9a-z]+))';
 const QA_RE_REPLACE = '#?&?' + QA_RE;
 
 export default {
-	shouldActivate: function(environmentId) {
-		const match = window.location.hash.match(new RegExp(QA_RE));
-		return match && environmentId === match[4];
-	},
-	activate: function() {
-		const match = window.location.hash.match(new RegExp(QA_RE));
+  shouldActivate: function(environmentId) {
+    const match = window.location.hash.match(new RegExp(QA_RE));
+    return match && environmentId === match[4];
+  },
+  activate: function() {
+    const match = window.location.hash.match(new RegExp(QA_RE));
 
-		if (match) {
-			const token = match[1];
+    if (match) {
+      const token = match[1];
 
-			window.evolv.store('candidateToken', token, true);
-			window.location.href = window.location.href.replace(new RegExp(QA_RE_REPLACE), '');
-		}
-	}
+      window.sessionStorage.setItem('evolv:candidateToken', token);
+      window.location.href = window.location.href.replace(new RegExp(QA_RE_REPLACE), '');
+    }
+  }
 };
