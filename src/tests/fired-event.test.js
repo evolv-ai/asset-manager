4a3540ae217c111788c97070cf02b884d743ff1d
Alireza Kasaaian <alirezakasaaian@gmail.com>
2022-01-05 12:50:34 +0000

    feat(ap-907): storing fired events in storage and local context

---
+++ src/tests/fired-event.test.js
@@ -0,0 +1,45 @@
+import * as assert from 'assert';
+
+import EvolvMock from './mocks/evolv.mock.js';
+import firedEvents, { eventHandler } from '../fired-events.js';
+
+describe('firedEvents', () => {
+    beforeEach(() => {
+        function mockStorage() {
+            var storage = {};
+            return {
+                setItem: function(key, value) {
+                    storage[key] = value || '';
+                },
+                getItem: function(key) {
+                    return storage[key];
+                }
+            };
+        };
+        global.sessionStorage = mockStorage();
+    })
+    afterEach(() => {
+        global.sessionStorage;
+    });
+    it('should set context from session', () => {
+        global.sessionStorage.setItem('evolv:fe', JSON.stringify({example: true}));
+        const client = new EvolvMock()
+        firedEvents(client)
+        assert.strictEqual(client.context.localContext.fired_events.example, true);
+    })
+    it('should set local context and session when an event is emitted', () => {
+        const client = new EvolvMock()
+
+        eventHandler(client, 'example')
+        assert.strictEqual(client.context.localContext['fired_events.example'], true);
+        assert.strictEqual(global.sessionStorage.getItem('evolv:fe'), '{"example":true}');
+    })
+    it('should extend session storage when an event is emitted', () => {
+        global.sessionStorage.setItem('evolv:fe', JSON.stringify({example: true}));
+        const client = new EvolvMock()
+
+        eventHandler(client, 'example2')
+        assert.strictEqual(client.context.localContext['fired_events.example2'], true);
+        assert.strictEqual(global.sessionStorage.getItem('evolv:fe'), '{"example":true,"example2":true}');
+    })
+})
