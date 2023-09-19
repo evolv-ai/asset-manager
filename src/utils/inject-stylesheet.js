2d55e27ea9b39297eb691b424bfd4a59a8ec93f0
robertsevernsentient <robert.severn@evolv.ai>
2023-09-19 08:54:51 -0700

    fix: some sites remove our css file
    it looks timing related - as it doesn't happen if we load a little later. Likely React wiping the dom of changes
    
    This change adds a mutation observer to the parent of the css file. then removes it after 10s
    
    AP-2244

--- src/utils/inject-stylesheet.js
+++ src/utils/inject-stylesheet.js
@@ -1,3 +1,42 @@
+export const MAX_RETRIES = 3;
+const MAX_REPLACE_TIME = 10000;
+
+/**
+ * @param {string} endpoint
+ * @param {string} env
+ * @param {number} version
+ * @param {string} uid
+ * @param {string} [cid]
+ *
+ * @returns {void}
+ */
+export function injectStylesheetWithAutoReplace(endpoint, env, version, uid, cid, attempt = 0) {
+	injectStylesheet(endpoint, env, version, uid, cid)
+	const url = getStyleSheetUrl(endpoint, env, version, uid, cid);
+	const cssFile = getCSSFile(url);
+
+	if (cssFile && window.MutationObserver) {
+		const observer = new window.MutationObserver(function (mutationList) {
+			mutationList.forEach(function (mutation) {
+				mutation.removedNodes.forEach(function (node) {
+					if (node.nodeName === 'LINK' && node.href === url && attempt < MAX_RETRIES) {
+						console.warn('Evolv AI: CSS removed, re-adding');
+						observer.disconnect();
+						injectStylesheetWithAutoReplace(endpoint, env, version, uid, cid, attempt + 1);
+					}
+				});
+			});
+		});
+		observer.observe(getCSSFile(url).parentNode, {
+			childList: true
+		});
+
+		setTimeout(function () {
+			observer.disconnect();
+		}, MAX_REPLACE_TIME);
+	}
+}
+
 /**
  * @param {string} endpoint
  * @param {string} env
@@ -8,12 +47,19 @@
  * @returns {void}
  */
 export function injectStylesheet(endpoint, env, version, uid, cid) {
-	const queryParams = (cid) ? '?previewcid=' + cid : '';
-
 	const stylesheet = document.createElement('link');
 	stylesheet.setAttribute('rel', 'stylesheet');
 	stylesheet.setAttribute('type', 'text/css');
-	stylesheet.setAttribute('href', endpoint.replace(/\/$/, '') + '/v' + version + '/' + env + '/' + uid + '/assets.css' + queryParams);
+	stylesheet.setAttribute('href', getStyleSheetUrl(endpoint, env, version, uid, cid));
 
 	document.head.appendChild(stylesheet);
+}
+
+function getStyleSheetUrl(endpoint, env, version, uid, cid) {
+	const queryParams = (cid) ? '?previewcid=' + cid : '';
+	return endpoint.replace(/\/$/, '') + '/v' + version + '/' + env + '/' + uid + '/assets.css' + queryParams;
+}
+
+function getCSSFile(url) {
+	return document.querySelector('link[href="' + url + '"]')
 }
