a9e7ecf07e928f4dd281b62ddd9b030ee104e1ff
robertsevernsentient <robert.severn@evolv.ai>
2023-09-19 09:13:24 -0700

    fix: comment to explain why
    AP-2244

--- src/utils/inject-stylesheet.js
+++ src/utils/inject-stylesheet.js
@@ -1,6 +1,7 @@
 export const MAX_RETRIES = 3;
 const MAX_REPLACE_TIME = 10000;
 
+// Some React sites will remove the stylesheet from the DOM, so we need to re-insert it
 /**
  * @param {string} endpoint
  * @param {string} env
